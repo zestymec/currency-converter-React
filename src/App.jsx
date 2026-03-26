ba4ba41376b71cdb47bfcfb0570a6d570429f0e5
M Umer Aziz <m.umer394aziz@gmail.com>
2026-03-26 11:42:59 +0500

    Change background image URL in App component

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
     <div
         className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
         style={{
-            backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
+            backgroundImage: `url('https://images.pexels.com/photos/15286/pexels-photo.jpg')`,
         }}
     >
         <div className="w-full">
